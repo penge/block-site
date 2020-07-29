88d790e62fcf83a5c89f319a1070be45e30e54f9
penge <ppenge@gmail.com>
2020-07-29 17:05:59 +0200

    Simplify all logic
    - Remove global.js and don't close already open tabs after adding them to blocked list; also remove any saving of tab ids, which was causing QUOTA_BYTES_PER_ITEM reached quota error before when saving many tab ids to sync storage
    
    - Simplify background.js and options.js
    - Improve options.html
    - Disable spellcheck for #textarea

--- background.js
+++ background.js
@@ -1,37 +1,31 @@
 "use strict";
 
-/* global chrome */
-/* global URL */
+/* global chrome, URL */
 
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.local.set({
-    enabled: true,
-    blocked: [
-      "facebook.com",
-      "instagram.com",
-      "youtube.com",
-      "twitter.com",
-      "reddit.com"
-    ],
-    tabs: {}
+  chrome.storage.local.get(["blocked", "enabled"], function (local) {
+    if (!Array.isArray(local.blocked)) {
+      chrome.storage.local.set({ blocked: [] });
+    }
+
+    if (typeof local.enabled !== "boolean") {
+      chrome.storage.local.set({ enabled: false });
+    }
   });
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
-  if (!tabId || !changeInfo.url || !changeInfo.url.startsWith("http")) {
+  const url = changeInfo.pendingUrl || changeInfo.url;
+  if (!url || !url.startsWith("http")) {
     return;
   }
 
-  chrome.storage.local.get(["enabled", "tabs"], function (result) {
-    // Store the tab
-    try {
-      result.tabs[tabId] = new URL(changeInfo.url).hostname;
-      chrome.storage.local.set({ "tabs": result.tabs }, function () {
-        if (result.enabled) {
-          chrome.extension.getBackgroundPage().removeTabs();
-        }
-      });
+  const hostname = new URL(url).hostname;
+
+  chrome.storage.local.get(["blocked", "enabled"], function (local) {
+    const { blocked, enabled } = local;
+    if (Array.isArray(blocked) && enabled && blocked.find(domain => hostname.includes(domain))) {
+      chrome.tabs.remove(tabId);
     }
-    catch (err) { return err; }
   });
 });
