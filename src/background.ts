7a9a92918dede2eec94eca925391f0c923608bc1
penge <ppenge@gmail.com>
2023-06-19 11:59:35 +0200

    Allow to block URLs navigated by browser history
    - add "tabs" permission
    Note: "history" permission isn't suitable as it is global (per any tab),
    doesn't have knowledge of tabId
    
    - optimize execution time by having prepared __enabled and __blocked
    (thus not loading every time from storage)

--- src/background.ts
+++ src/background.ts
@@ -1,18 +1,56 @@
 import initStorage from "./storage/init";
-import blockSite from "./helpers/block-site";
+import storage from "./storage";
 import createContextMenu from "./helpers/create-context-menu";
+import blockSite from "./helpers/block-site";
 
-initStorage(createContextMenu);
+let __enabled: boolean;
+let __blocked: string[];
+
+initStorage(() => {
+  createContextMenu();
+
+  storage.get(["enabled", "blocked"], ({ enabled, blocked }) => {
+    __enabled = enabled;
+    __blocked = blocked;
+  });
+
+  chrome.storage.local.onChanged.addListener((changes) => {
+    if (changes["enabled"]) {
+      __enabled = changes["enabled"].newValue as boolean;
+    }
+
+    if (changes["blocked"]) {
+      __blocked = changes["blocked"].newValue as string[];
+    }
+  });
+});
 
 chrome.action.onClicked.addListener(() => {
   chrome.runtime.openOptionsPage();
 });
 
 chrome.webNavigation.onBeforeNavigate.addListener((details) => {
-  const { tabId, url, timeStamp, frameId } = details;
+  if (!__enabled || !__blocked.length) {
+    return;
+  }
+
+  const { tabId, url, frameId } = details;
   if (!url || !url.startsWith("http") || frameId !== 0) {
     return;
   }
 
-  blockSite({ tabId, url, timeStamp });
+  blockSite({ blocked: __blocked, tabId, url });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  if (!tabId || !__enabled || !__blocked.length) {
+    return;
+  }
+
+  const { status, url } = changeInfo;
+  if (status !== "loading" || !url || !url.startsWith("http")) {
+    return;
+  }
+
+  blockSite({ blocked: __blocked, tabId, url });
 });
--- src/helpers/block-site.ts
+++ src/helpers/block-site.ts
@@ -4,50 +4,49 @@ import * as counterHelper from "./counter";
 import getBlockedUrl from "./get-blocked-url";
 
 interface BlockSiteOptions {
+  blocked: string[]
   tabId: number
   url: string
-  timeStamp: number
 }
 
 export default (options: BlockSiteOptions) => {
-  const { tabId, url, timeStamp } = options;
+  const { blocked, tabId, url } = options;
+  if (!blocked.length || !tabId || !url.startsWith("http")) {
+    return;
+  }
 
-  storage.get(["enabled", "blocked"], ({ enabled, blocked }) => {
-    if (!enabled || blocked.length === 0) {
-      return;
-    }
-
-    const foundRule = findRule(url, blocked);
-    if (!foundRule || foundRule.type === "allow") {
-      storage.get(["counter"], ({ counter }) => {
-        counterHelper.flushObsoleteEntries({ blocked, counter });
-        storage.set({ counter });
-      });
-      return;
-    }
-
-    storage.get(["counter", "counterShow", "counterPeriod", "resolution"], ({ counter, counterShow, counterPeriod, resolution }) => {
+  const foundRule = findRule(url, blocked);
+  if (!foundRule || foundRule.type === "allow") {
+    storage.get(["counter"], ({ counter }) => {
       counterHelper.flushObsoleteEntries({ blocked, counter });
-      const count = counterHelper.add(foundRule.path, timeStamp, {
-        counter,
-        countFromTimeStamp: counterHelper.counterPeriodToTimeStamp(counterPeriod, new Date().getTime()),
-      });
       storage.set({ counter });
-
-      switch (resolution) {
-      case "CLOSE_TAB":
-        chrome.tabs.remove(tabId);
-        break;
-      case "SHOW_BLOCKED_INFO_PAGE": {
-        chrome.tabs.update(tabId, {
-          url: getBlockedUrl({
-            url,
-            rule: foundRule.path,
-            countParams: counterShow ? { count, period: counterPeriod } : undefined },
-          )},
-        );
-        break;
-      }}
     });
+    return;
+  }
+
+  storage.get(["counter", "counterShow", "counterPeriod", "resolution"], ({ counter, counterShow, counterPeriod, resolution }) => {
+    counterHelper.flushObsoleteEntries({ blocked, counter });
+
+    const timeStamp = Date.now();
+    const count = counterHelper.add(foundRule.path, timeStamp, {
+      counter,
+      countFromTimeStamp: counterHelper.counterPeriodToTimeStamp(counterPeriod, new Date().getTime()),
+    });
+    storage.set({ counter });
+
+    switch (resolution) {
+    case "CLOSE_TAB":
+      chrome.tabs.remove(tabId);
+      break;
+    case "SHOW_BLOCKED_INFO_PAGE": {
+      chrome.tabs.update(tabId, {
+        url: getBlockedUrl({
+          url,
+          rule: foundRule.path,
+          countParams: counterShow ? { count, period: counterPeriod } : undefined },
+        )},
+      );
+      break;
+    }}
   });
 };
